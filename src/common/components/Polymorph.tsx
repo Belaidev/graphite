b3a831c5c8ffc658c9ff152bcb142cc60598cffb
Belaidev <belaidev@protonmail.com>
2024-01-08 05:58:33 -0500

    Common: modify 'Polymorph' component implementation

--- src/common/components/Polymorph.tsx
+++ src/common/components/Polymorph.tsx
@@ -1,33 +1,22 @@
 import { combineProps } from '@solid-primitives/props';
-import { createChildren } from 'common/primitives';
-import { JSXElement, Show, ValidComponent, createMemo, on } from 'solid-js';
+import { JSXElement, ValidComponent, createMemo } from 'solid-js';
 import { Dynamic, DynamicProps } from 'solid-js/web';
 
-export type PolymorphProps<T extends ValidComponent = ValidComponent> = DynamicProps<T> & {
+export type PolymorphProps<T extends ValidComponent> = DynamicProps<T> & {
+	as?: JSXElement;
 	children?: JSXElement;
 };
 
 export function Polymorph<T extends ValidComponent>(props: PolymorphProps<T>): JSXElement {
-	const children = createChildren<AsProps | JSXElement>(() => props.children);
-
-	const isAsProps = (child: AsProps | JSXElement | undefined): child is AsProps =>
-		!!child && typeof child === 'object' && 'component' in child;
-
-	const propsCombined = createMemo(
-		on(children, ([child]) => (isAsProps(child) ? combineProps(child, props) : props))
+	const propsCombined = createMemo<PolymorphProps<ValidComponent>>(() =>
+		props.as ? combineProps(props.as as unknown as AsProps<ValidComponent>, props) : props
 	);
 
-	return (
-		// @ts-expect-error
-		<Dynamic {...propsCombined()}>
-			<Show when={isAsProps(children()[0])} fallback={children() as JSXElement}>
-				{(children()[0] as AsProps).children}
-			</Show>
-		</Dynamic>
-	);
+	return <Dynamic {...propsCombined()}>{props.children}</Dynamic>;
 }
 
-export type AsProps<T extends ValidComponent = ValidComponent> = DynamicProps<T> & {
+export type AsProps<T extends ValidComponent> = Omit<DynamicProps<T>, 'component'> & {
+	component?: JSXElement;
 	children?: JSXElement;
 };
 
