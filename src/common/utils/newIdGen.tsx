97b550087bef6df295ea5ac6d2ce1ee2a4929082
Belaidev <belaidev@protonmail.com>
2024-01-08 06:04:50 -0500

    Utils: add 'newIdGen' function

---
+++ src/common/utils/newIdGen.tsx
@@ -0,0 +1,11 @@
+export function newIdGen(seq = 1): () => number {
+	let id = seq;
+	const genId = () => {
+		if (id >= Number.MAX_SAFE_INTEGER)
+			throw new Error(
+				'ID value is greater than or equal to the maximum safe integer in JavaScript. Cannot generate new ID.'
+			);
+		return id++;
+	};
+	return genId;
+}
